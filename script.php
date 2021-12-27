55695e5833183544679d3a6745d842661f0c8e0d
2Ag@19920306 <Ag.taoufik@gmail.com>
2021-12-27 16:37:09 +0300

    V1

---
+++ script.php
@@ -0,0 +1,36 @@
+<?php
+header("Location: index.php");
+
+$countfiles = count($_FILES['uploadedFile']['name']);
+
+for ($i = 0; $i < $countfiles; $i++) {
+
+    $fileType = $_FILES['uploadedFile']['type'][$i];
+
+    $filename = $_FILES['uploadedFile']['name'][$i];
+
+    $tmp_name = $_FILES['uploadedFile']['tmp_name'][$i];
+
+    $fileNameCmps = explode(".", $filename);
+
+    if ($fileType === "application/octet-stream") {
+
+        $ArrayData = include "$tmp_name";
+
+    } elseif ($fileType === "application/json") {
+
+        $ArrayData = json_decode(file_get_contents($tmp_name), true);
+    }
+
+    $fp = fopen("./Excels/$fileNameCmps[0].xls", "w");
+
+    fputcsv($fp, ['Value', 'English']);
+
+    foreach ($ArrayData as $key => $value) {
+        fputcsv($fp, [$value, $key]);
+    }
+
+    fclose($fp);
+
+    unlink($tmp_name);
+}
